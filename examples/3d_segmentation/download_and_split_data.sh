853618da66b95bdf502600b9376823fcc07672e4
nbrodic88 <48680700+nbrodic88@users.noreply.github.com>
2019-08-15 17:16:35 +0200

    Update download_and_split_data.sh

--- examples/3d_segmentation/download_and_split_data.sh
+++ examples/3d_segmentation/download_and_split_data.sh
@@ -8,18 +8,12 @@
 #Download  https://shapenet.cs.stanford.edu/iccv17/ competition data.
 #We re-split the Train/Validation data 50-50 to increase the size of the validation set.
 
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/train_data.zip
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/train_label.zip
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/val_data.zip
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/val_label.zip
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/test_data.zip
-wget https://shapenet.cs.stanford.edu/iccv17/partseg/test_label.zip
-unzip train_data.zip
-unzip train_label.zip
-unzip val_data
-unzip val_label
-unzip test_data.zip
-unzip test_label.zip
+wget https://www.dropbox.com/s/9to7yq1nmp5q4mf/Train-Track4.zip?dl=0 -O Train-Track4.zip
+wget https://www.dropbox.com/s/8lyklqxiv3uijg0/Train-Track4-Truth.zip?dl=0 -O Train-Track4-Truth.zip
+wget https://www.dropbox.com/s/kja8sc464kzxsoy/Test-Track4.zip?dl=0 -O Test-Track4.zip
+unzip Train-Track4.zip
+unzip Train-Track4-Truth.zip
+unzip Test-Track4.zip
 for x in train_val test; do 
     for y in 02691156 02773838 02954340 02958343 03001627 03261776 03467517 03624134 03636649 03642806 03790512 03797390 03948459 04099429 04225987 04379243; do 
         mkdir -p $x/$y
